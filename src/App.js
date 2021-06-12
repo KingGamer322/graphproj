01d51d5f4c3549f0e870d8d6398a69dfb0a34fc6
DaemonMarshall <eminem.marshallbrusemathers3@gmail.com>
2021-06-12 22:33:07 +0500

    verstka

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import peepologo from './smallestpeepo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Navigation">
+          <img className="logo" src={peepologo} alt="peepo on chair"></img>
+        <div className="graph1">
+          <select className="graphSelect">Доска 1</select>
+        </div>
+        <div className="graph2">
+          <select className="graphSelect">Доска 2</select>
+        </div>
+        <div className="Profile">
+          <p>-</p>
+        </div>
+      </div>
+      <div className="SideAndGraph">
+        <div className="SideBar">
+            <button className="insideButton"><span className="textinside"><img className="logoinside" src={peepologo} alt="peepo on chair"/>Создать</span></button>
+            <button className="insideButton"><span className="textinside"><img className="logoinside" src={peepologo} alt="peepo on chair"/>Удалить</span></button>
+            <button className="insideButton"><span className="textinside"><img className="logoinside" src={peepologo} alt="peepo on chair"/>Изменить</span></button>
+        </div>
+        <div className="Graph"></div>
+      </div>
     </div>
   );
 }
