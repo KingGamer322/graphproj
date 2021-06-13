f872e1b9ee3b9554d266150dcd04880be0840a5c
DaemonMarshall <eminem.marshallbrusemathers3@gmail.com>
2021-06-13 22:21:12 +0500

    Design without logic

--- src/App.js
+++ src/App.js
@@ -2,26 +2,74 @@ import './App.css';
 import React from 'react';
 import peepologo from './smallestpeepo.png';
 
+
 function App() {
   return (
     <div className="App">
       <div className="Navigation">
           <img className="logo" src={peepologo} alt="peepo on chair"></img>
         <div className="graph1">
-          <select className="graphSelect">Доска 1</select>
+          <button className="graphButton">Доска 1</button>
         </div>
         <div className="graph2">
-          <select className="graphSelect">Доска 2</select>
+          <button className="graphButton second">Доска 2</button>
         </div>
-        <div className="Profile">
-          <p>-</p>
+        <div className="graph3">
+          <button className="graphButton third">+</button>
         </div>
+          <img className="logo Profile" src={peepologo} alt="peepo on chair"></img>
       </div>
       <div className="SideAndGraph">
         <div className="SideBar">
-            <button className="insideButton"><span className="textinside"><img className="logoinside" src={peepologo} alt="peepo on chair"/>Создать</span></button>
-            <button className="insideButton"><span className="textinside"><img className="logoinside" src={peepologo} alt="peepo on chair"/>Удалить</span></button>
-            <button className="insideButton"><span className="textinside"><img className="logoinside" src={peepologo} alt="peepo on chair"/>Изменить</span></button>
+            <button className="insideButton top"><span className="imageAndText"><img className="logoinside" src={peepologo} alt="peepo on chair"/><span class="textinside">Создать</span></span></button>
+            <div className="dropdownElements">
+              <button className="liButton first">Вершину</button>
+              <button className="liButton">Ребро</button>
+              <button className="liButton">Путь</button>
+              <button className="liButton last">Краткий путь от начала до вершины</button>
+            </div>
+            <button className="insideButton"><span className="imageAndText"><img className="logoinside" src={peepologo} alt="peepo on chair"/><span class="textinside">Удалить</span></span></button>
+            <div className="dropdownElements2">
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Вершину</button></li>
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Ребро</button></li>
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Путь</button></li>
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Краткий путь от начала до вершины</button></li>
+            </div>
+            <button className="insideButton"><span className="imageAndText"><img className="logoinside" src={peepologo} alt="peepo on chair"/><span class="textinside">Изменить</span></span></button>
+            <div className="dropdownElements2">
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Вершину</button></li>
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Ребро</button></li>
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Путь</button></li>
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Краткий путь от начала до вершины</button></li>
+            </div>
+            <button className="insideButton"><span className="imageAndText"><img className="logoinside" src={peepologo} alt="peepo on chair"/><span class="textinside">Удалить</span></span></button>
+            <div className="dropdownElements2">
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Вершину</button></li>
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Ребро</button></li>
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Путь</button></li>
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Краткий путь от начала до вершины</button></li>
+            </div>
+            <button className="insideButton"><span className="imageAndText"><img className="logoinside" src={peepologo} alt="peepo on chair"/><span class="textinside">Экспортировать</span></span></button>
+            <div className="dropdownElements2">
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Вершину</button></li>
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Ребро</button></li>
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Путь</button></li>
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Краткий путь от начала до вершины</button></li>
+            </div>
+            <button className="insideButton"><span className="imageAndText"><img className="logoinside" src={peepologo} alt="peepo on chair"/><span class="textinside wide">Обновить представление</span></span></button>
+            <div className="dropdownElements">
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Вершину</button></li>
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Ребро</button></li>
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Путь</button></li>
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Краткий путь от начала до вершины</button></li>
+            </div>
+            {/* <button className="insideButton"><span className="imageAndText"><img className="logoinside" src={peepologo} alt="peepo on chair"/><span class="textinside wide">Что-то новое и большое</span></span></button> */}
+            <div className="dropdownElements2">
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Вершину</button></li>
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Ребро</button></li>
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Путь</button></li>
+              <li className="listElement" href="http://localhost:3000/"><button className="liButton">Краткий путь от начала до вершины</button></li>
+            </div>
         </div>
         <div className="Graph"></div>
       </div>
